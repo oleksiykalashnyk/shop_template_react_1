11d8edb6ff745e33477c86a6ba8a928c2ee16bfc
oleksiykalashnyk <68416432+oleksiykalashnyk@users.noreply.github.com>
2021-12-27 01:49:21 +0100

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import LayoutFirst from "./layouts/LayoutFirst";
 import Loader from './components/loader/Loader'
 import BlogList from "./pages/blog_list/BlogList";
 import Post from './pages/post/Post';
+import ScrollToTop from "./components/scroll_to_top/ScrollToTop";
 
 const Home = lazy(() => import('./pages/home/Home'));
 const Product = lazy(() => import('./pages/product/Product'));
@@ -15,25 +16,29 @@ const App = () => {
     return (
         <LayoutFirst>
             <Suspense fallback={<Loader/>}>
-                <Routes>
-                    <Route path='/' exact element={<Home/>}/>
-                    <Route path='/blog' element={<BlogList/>}/>
-                    <Route path='/post/:id' element={<Post/>}/>
+                <ScrollToTop>
+                    <Routes>
+                        <Route path='/' exact element={<Home/>}/>
+                        <Route path='/blog' element={<BlogList/>}/>
+                        <Route path='/post/:id' element={<Post/>}/>
 
-                    <Route path='/product/:id' element={<Product/>}/>
-                    <Route
-                        path="*"
-                        element={
-                            <main style={{padding: "1rem", textAlign: "center"}}>
-                                <h1>404</h1>
-                                <p>There's nothing here!</p>
-                            </main>
-                        }
-                    />
-                </Routes>
+                        <Route path='/product/:id' element={<Product/>}/>
+                        <Route
+                            path="*"
+                            element={
+                                <main style={{padding: "1rem", textAlign: "center"}}>
+                                    <h1>404</h1>
+                                    <p>There's nothing here!</p>
+                                </main>
+                            }
+                        />
+                    </Routes>
+                </ScrollToTop>
+
             </Suspense>
         </LayoutFirst>
-    );
+    )
+        ;
 };
 
 export default App;
